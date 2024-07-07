5210df534b7fe367968c5857aa21e8103b9ddea1
clmb <mclmbo@pm.me>
2024-07-07 20:22:06 +0100

    hehe

---
+++ script.js
@@ -0,0 +1,19 @@
+async function fetchIPData() {
+  try {
+    const response = await fetch("http://edns.ip-api.com/json");
+    if (!response.ok) {
+      throw new Error("Network response was not ok " + response.statusText);
+    }
+    const data = await response.json();
+    document.getElementById("ip-data").textContent = JSON.stringify(
+      data,
+      null,
+      2
+    );
+  } catch (error) {
+    console.error("There has been a problem with your fetch operation:", error);
+    document.getElementById("ip-data").textContent = "Error fetching data";
+  }
+}
+
+fetchIPData();
